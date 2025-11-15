8f2f339a44d2db7b9282fde587bf77c22dee4fe6
Javier Artigas Alfaro <j-a-a1996@hotmail.es>
2025-11-15 23:08:23 +0100

    refactor: actualizar script de versión y mejorar manejo de argumentos

--- scripts/update-module-version.js
+++ scripts/update-module-version.js
@@ -1,10 +1,26 @@
 const fs = require("fs");
 
-const pkg = JSON.parse(fs.readFileSync("./package.json", "utf8"));
-const module = JSON.parse(fs.readFileSync("./module.json", "utf8"));
+// Argumento recibido desde Github Action
+const version = process.argv[2];
 
-module.version = pkg.version;
+if (!version) {
+    console.error("No se recibió versión en update-module-version.js");
+    process.exit(1);
+}
 
-fs.writeFileSync("./module.json", JSON.stringify(module, null, 2));
+console.log("Aplicando versión:", version);
 
-console.log(`Versión sincronizada: ${pkg.version}`);
+const modulePath = "./module.json";
+const moduleData = JSON.parse(fs.readFileSync(modulePath, "utf8"));
+
+moduleData.version = version;
+
+// Actualizar download URL y manifest URL automáticamente
+const repo = process.env.GITHUB_REPOSITORY; // ej: "JavierArtigas/magic-random-items"
+
+moduleData.download = `https://github.com/${repo}/releases/download/v${version}/module.zip`;
+moduleData.manifest = `https://raw.githubusercontent.com/${repo}/main/module.json`;
+
+fs.writeFileSync(modulePath, JSON.stringify(moduleData, null, 2));
+
+console.log("module.json actualizado correctamente");
